aa26697aeab13b380aa47cfa93915ba398a34187
Peerapon Wechsuwanmanee <peerapon.w@gmail.com>
2019-09-08 08:43:44 +0200

    Add files via upload
    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import numpy as np
+# Flask
+from flask import Flask, request, render_template
+
+# define app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__=='__main__':
+	app.run()
